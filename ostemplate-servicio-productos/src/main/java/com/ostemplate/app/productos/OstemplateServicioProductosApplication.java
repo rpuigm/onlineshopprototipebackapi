77c56260af01dd55f299292092a6f6b267329af9
RubenPuigMoreno <ruben.puig@empresas.justicia.es>
2021-07-31 16:53:27 +0200

    feat: creando servicio sobre productos

---
+++ ostemplate-servicio-productos/src/main/java/com/ostemplate/app/productos/OstemplateServicioProductosApplication.java
@@ -0,0 +1,13 @@
+package com.ostemplate.app.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstemplateServicioProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OstemplateServicioProductosApplication.class, args);
+	}
+
+}
