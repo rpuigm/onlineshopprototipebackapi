47bdafe394c8aa8139b54459d9f95299b449ec9c
RubenPuigMoreno <ruben.puig@empresas.justicia.es>
2021-07-31 21:21:49 +0200

    feat: iniciando servicios producto y clientes

---
+++ ostemplate-servicio-clientes/src/main/java/com/ostemplate/app/clientes/OstemplateServicioClientesApplication.java
@@ -0,0 +1,13 @@
+package com.ostemplate.app.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstemplateServicioClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OstemplateServicioClientesApplication.class, args);
+	}
+
+}
---
+++ ostemplate-servicio-productos/src/main/java/com/ostemplate/app/productos/controllers/ProductoController.java
@@ -0,0 +1,40 @@
+package com.ostemplate.app.productos.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ostemplate.app.productos.models.entity.Producto;
+import com.ostemplate.app.productos.models.service.IProductoService;
+
+@RestController
+public class ProductoController {
+	
+	@Value("${server.port}")
+	private Integer port;
+	
+	@Autowired
+	private IProductoService productoService;
+	
+	@GetMapping("/listaproductos")
+	public List<Producto> listar(){
+		return productoService.findAll().stream().map(producto ->{
+			producto.setPort(port);
+			return producto;
+		}).collect(Collectors.toList());
+	}
+	
+	@GetMapping("/producto/{id}")
+	public Producto detalle(@PathVariable Long id) {
+		Producto producto = productoService.findById(id);
+		producto.setPort(port);
+		return producto;
+	}
+
+}
