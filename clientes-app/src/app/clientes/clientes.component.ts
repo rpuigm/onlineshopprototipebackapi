d44df53b18e87d5f82a37253aac1f7087bfe72bd
rpuigm <ruben.puig.moreno@gmail.com>
2021-08-07 13:15:00 +0200

    feat: añadimos servicio cliente sincrono, el bootstrap no hace bien el responsive

---
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -0,0 +1,23 @@
+import { ClienteService } from './cliente.service';
+
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+
+})
+export class ClientesComponent implements OnInit {
+
+
+  clientes!: Cliente[];
+
+  constructor(private clienteService: ClienteService){}
+
+  ngOnInit(): void {
+    this.clientes = this.clienteService.getClientes();
+  }
+
+}
---
+++ clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+
+  {id: 1, nombre: 'Ruben', apellido:'Puig', email:'test@test.com', alta:'25-07-2021'},
+  {id: 1, nombre: 'Cliente1', apellido:'Puig', email:'testrp@test.com', alta:'25-01-2021'}  ,
+  {id: 1, nombre: 'Cliente2', apellido:'Apellido2', email:'test1@test.com', alta:'25-02-2021'} ,
+  {id: 1, nombre: 'Cliente2', apellido:'Apellido2', email:'test1@test.com', alta:'25-03-2021'} ,
+  {id: 1, nombre: 'Cliente2', apellido:'Apellido2', email:'test1@test.com', alta:'25-04-2021'},
+  {id: 1, nombre: 'Cliente2', apellido:'Apellido2', email:'test1@test.com', alta:'25-04-2021'}  ];
