de149337844a1f4d9dc9380617e1d691e4b88390
rpuigm <ruben.puig.moreno@gmail.com>
2021-08-26 18:33:48 +0200

    refactor: making basic

---
+++ ostemplate/ostemplate-servicio-personas/src/test/java/com/ostemplate/app/personas/OstemplateServicioPersonasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ostemplate.app.personas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OstemplateServicioPersonasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
