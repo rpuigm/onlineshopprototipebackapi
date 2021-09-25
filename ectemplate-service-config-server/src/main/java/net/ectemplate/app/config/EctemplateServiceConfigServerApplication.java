282606da07bfe8fc438a600c3a9cbfabe15103b7
RubenPuigMoreno <ruben.puig@empresas.justicia.es>
2021-09-25 12:38:48 +0200

    feat: añadimos config server

---
+++ ectemplate-service-config-server/src/main/java/net/ectemplate/app/config/EctemplateServiceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package net.ectemplate.app.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EctemplateServiceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EctemplateServiceConfigServerApplication.class, args);
+	}
+
+}
---
+++ ectemplate-service-config-server/src/test/java/net/ectemplate/app/config/EctemplateServiceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ectemplate.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EctemplateServiceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
