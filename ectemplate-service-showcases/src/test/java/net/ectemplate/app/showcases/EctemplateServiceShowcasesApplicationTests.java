3902db94ffcbc634b2ba1cd8eb902fb26cb193a9
RubenPuigMoreno <ruben.puig@empresas.justicia.es>
2021-09-25 12:27:22 +0200

    feat: añadidos servicios showcases y gateway

---
+++ ectemplate-service-showcases/src/test/java/net/ectemplate/app/showcases/EctemplateServiceShowcasesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ectemplate.app.showcases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EctemplateServiceShowcasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
