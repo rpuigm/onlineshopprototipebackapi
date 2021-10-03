4e4b98cad3d2892e49e3a1125e6af01c58145459
RubenPuigMoreno <ruben.puig@empresas.justicia.es>
2021-10-03 14:25:27 +0200

    F001 - feat: formulario y método creado

--- clientes-app/src/app/clientes/form.component.ts
+++ clientes-app/src/app/clientes/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente'
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  public cliente:Cliente = new Cliente()
+  public titulo:string = "Alta de Cliente"
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public create(): void{
+    console.log("Listo!!")
+    console.log(this.cliente)
   }
 
 }
