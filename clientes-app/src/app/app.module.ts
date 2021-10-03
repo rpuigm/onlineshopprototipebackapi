b5452b3039feb5594ad2d1d75074881ddb08d208
RubenPuigMoreno <ruben.puig@empresas.justicia.es>
2021-10-03 14:34:54 +0200

    F001 - feat: añadido enrutamiento, link página principal no funciona formato no se ve bien

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -15,6 +15,7 @@ const routes: Routes = [
   {path:'', redirectTo:'/clientes', pathMatch: 'full'},
   {path:'directivas', component: DirectivaComponent},
   {path:'clientes', component: ClientesComponent},
+  {path:'clientes/form', component: FormComponent},
 ];
 
 @NgModule({
