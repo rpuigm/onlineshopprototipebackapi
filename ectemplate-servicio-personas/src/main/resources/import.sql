c2b8b572d49357bdecddc6bb2c527372d2b07378
RubenPuigMoreno <ruben.puig@empresas.justicia.es>
2021-11-06 22:09:29 +0100

    feat: comenzamos con la seguridad

--- ectemplate-servicio-personas/src/main/resources/import.sql
+++ ectemplate-servicio-personas/src/main/resources/import.sql
@@ -1,2 +1,9 @@
 INSERT INTO usuarios (nombre, apellidos, edad, username, password) VALUES ('nombre1', 'apellidos1', 10, 'usuario1', 'contra1');
 INSERT INTO usuarios (nombre, apellidos, edad, username, password) VALUES ('nombre2', 'apellidos2', 20, 'usuario2', 'contra2');
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1,1);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2,2);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2,1);
