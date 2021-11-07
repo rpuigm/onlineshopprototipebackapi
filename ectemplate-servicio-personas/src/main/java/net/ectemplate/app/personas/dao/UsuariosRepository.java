5f2c4fa875e14c9f256ef7b16187988a494441f0
RubenPuigMoreno <ruben.puig@empresas.justicia.es>
2021-11-07 11:34:05 +0100

    feat: personalizamos la busqueda del username

--- ectemplate-servicio-personas/src/main/java/net/ectemplate/app/personas/dao/UsuariosRepository.java
+++ ectemplate-servicio-personas/src/main/java/net/ectemplate/app/personas/dao/UsuariosRepository.java
@@ -1,13 +1,19 @@
 package net.ectemplate.app.personas.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import net.ectemplate.app.personas.entities.Usuario;
 
 @RepositoryRestResource(path="usuarios")
 public interface UsuariosRepository extends PagingAndSortingRepository<Usuario, Long> {
 
-	public Usuario findByUsername(String username);
+	@RestResource(path="buscar-username")
+	public List<Usuario> findByUsername(@Param("nombreUsuario") String username);
+	
 	
 }
