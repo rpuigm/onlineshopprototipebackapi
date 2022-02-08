9f92b7c02cdc210018dae4538a9c8d3000a7619d
RubenPuigMoreno <ruben.puig.moreno@gmail.com>
2022-02-08 23:03:22 +0100

    feat: creamos clase dto

---
+++ ectemplate-servicio-personas/src/main/java/net/ectemplate/app/personas/dto/UsuarioDTO.java
@@ -0,0 +1,12 @@
+package net.ectemplate.app.personas.dto;
+
+import net.ectemplate.app.personas.entities.Usuario;
+
+public class UsuarioDTO extends Usuario {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
