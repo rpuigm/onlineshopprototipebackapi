c7babc9e2a68bbdbc49c95c296341fc896cdde89
RubenPuigMoreno <ruben.puig.moreno@gmail.com>
2022-10-15 14:26:17 +0200

    fix: fixing test

--- ectemplate-servicio-gateway/src/test/java/net/ectemplate/app/getway/EctemplateServicioGetwayApplicationTests.java
+++ ectemplate-servicio-gateway/src/test/java/net/ectemplate/app/getway/EctemplateServicioGetwayApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes=EctemplateServicioGetwayApplication.class)
-class EctemplateServicioGetwayApplicationTests {
+class EctemplateServicioGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
