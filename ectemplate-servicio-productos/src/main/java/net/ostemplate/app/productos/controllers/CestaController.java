d7c0a2694603a8a48bf50f18f9d1c7f602cbc754
Rubén Puig Moreno <48332388+rpuigm@users.noreply.github.com>
2024-02-10 13:44:40 +0000

    feat: CestaControllerTest added, CestaController modified

--- ectemplate-servicio-productos/src/main/java/net/ostemplate/app/productos/controllers/CestaController.java
+++ ectemplate-servicio-productos/src/main/java/net/ostemplate/app/productos/controllers/CestaController.java
@@ -12,34 +12,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import net.ostemplate.app.productos.mappers.CestaMapper;
 import net.ostemplate.app.productos.models.entity.Cesta;
-import net.ostemplate.app.productos.models.service.CestaServiceImpl;
+import net.ostemplate.app.productos.models.service.CestaServiceI;
 
 @RestController
 public class CestaController {
 
 	@Autowired
-	private CestaServiceImpl cestaServiceImpl;
+	private CestaServiceI cestaServiceI;
 	
 	@GetMapping("/cesta/recupera-cesta/{idUsuario}")
 	public Cesta recuperaCestaPorIdUsuario (@PathVariable Long idUsuario) {
-		return cestaServiceImpl.buscarCestaPorUsuarioId(idUsuario);
+		return cestaServiceI.buscarCestaPorUsuarioId(idUsuario);
 	}
 	
 	
 	@PostMapping("/cesta/actualiza-cesta")
 	public Cesta actualizaCesta(@RequestBody Cesta cesta) {
 		System.out.println("numero productos en cesta"+cesta.getProductoCantidad().size());
-		return cestaServiceImpl.actualizaCesta(CestaMapper.mapToCestaEntityFromCesta(cesta));
+		return cestaServiceI.actualizaCesta(CestaMapper.mapToCestaEntityFromCesta(cesta));
 	}
 	
 	@GetMapping("/cesta/listacestas")
 	public List<Cesta> listaCestas(){
-		return CestaMapper.mapToListCestaFromListCestaEntity(cestaServiceImpl.listaCestas()) ;
+		return CestaMapper.mapToListCestaFromListCestaEntity(cestaServiceI.listaCestas()) ;
 	}
 	
 	@DeleteMapping("/cesta/elimina-cesta")
 	public void recuperaCestaPorIdUsuario (@RequestBody Cesta cesta) {
-		cestaServiceImpl.eliminaCesta(CestaMapper.mapToCestaEntityFromCesta(cesta));
+		cestaServiceI.eliminaCesta(CestaMapper.mapToCestaEntityFromCesta(cesta));
 	}
 	
 }
