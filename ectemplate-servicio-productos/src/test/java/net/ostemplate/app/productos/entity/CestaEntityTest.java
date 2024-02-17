4b27b762f514836a7d3158a37a45476dcefd4116
Rubén Puig Moreno <48332388+rpuigm@users.noreply.github.com>
2024-02-17 10:01:08 +0000

    feat: CestaEntityTest class added

---
+++ ectemplate-servicio-productos/src/test/java/net/ostemplate/app/productos/entity/CestaEntityTest.java
@@ -0,0 +1,11 @@
+package net.ostemplate.app.productos.entity;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension; 
+
+@ExtendWith(MockitoExtension.class)
+public class CestaEntityTest {
+
+    
+
+}
