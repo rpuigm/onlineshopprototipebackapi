353979c26181cef7a444b68d7f13529cf41d303a
Rubén Puig Moreno <48332388+rpuigm@users.noreply.github.com>
2024-08-04 09:38:56 +0000

    feat: run file modifying

--- run.sh
+++ run.sh
@@ -9,9 +9,34 @@ echo $r
 # Config
 cd $r/ectemplate-service-config-server
 echo "Starting Config Service..."
-mvn -q clean spring-boot:run &
+mvn -q clean spring-boot:run 
 
 # Config
-cd $r/ectemplate-service-config-server
-echo "Starting Config Service..."
-mvn -q clean spring-boot:run &
+cd $r/ectemplate-servicio-eureka-server
+echo "Starting Eureka Service..."
+mvn -q clean spring-boot:run 
+
+# Config
+cd $r/ectemplate-service-oauth
+echo "Starting Oauth Service..."
+mvn -q clean spring-boot:run 
+
+# Config
+cd $r/ectemplate-servicio-peronas
+echo "Starting Personas Service..."
+mvn -q clean spring-boot:run 
+
+# Config
+cd $r/ectemplate-servicio-productos
+echo "Starting Productos Service..."
+mvn -q clean spring-boot:run 
+
+# Config
+cd $r/ectemplate-service-zuul-server
+echo "Starting Zuul Service..."
+mvn -q clean spring-boot:run 
+
+# Config
+cd $r/clientes-app
+echo "Starting Clientes App..."
+mvn -q clean spring-boot:run 
